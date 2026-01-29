9e66db58435a57128678fe547d01edb362de0d8d
Moshiur Rahman <m.sujon4u@gmail.com>
2026-01-30 00:32:10 +0600

    update one Loops all Condition

---
+++ Condition/Partice.php
@@ -0,0 +1,34 @@
+<?php
+if (condition) {
+    # code...
+}
+//ifelse--------------
+if (condition) {
+    # code...
+} else {
+    # code...
+}
+//ifelse if--------------
+if (condition) {
+    # code...
+} elseif (condition) {
+    # code...
+} else {
+    # code...
+}
+//switch--------------
+switch ($variable) {
+    case 'value1':
+        # code...
+        break;
+    case 'value2':
+        # code...
+        break;
+    default:
+        # code...
+        break;
+}
+//Ternary Operator--------------
+$result = (condition) ? 'value if true' : 'value if false';
+//Null Coalescing Operator--------------
+$value = $variable ?? 'default value';
