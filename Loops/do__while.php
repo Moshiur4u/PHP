3df7d7c9fa1bb178d0fc95cf5b95c7ff17b57008
Moshiur Rahman <m.sujon4u@gmail.com>
2026-02-08 00:14:56 +0600

    update comment Example in loops

--- Loops/do__while.php
+++ Loops/do__while.php
@@ -1,18 +1,22 @@
 <?php
+
+//Example 1-------
 $i = 5;
 do {
 	echo $i . "\n";
 	$i--;
 } while ($i > 0);
-
+//Example 2---------
 echo "<br>";
 $j = 0;
 do {
 	echo $j . "\n";
 	$j++;
 } while ($j < 10);
-echo "<br>";
 
+
+echo "<br>";
+//Example 3----------
 $choice = 0;
 do {
 	echo "Menu: 1=Start, 2=Continue, 0=Exit\n";
@@ -22,6 +26,7 @@ do {
 
 echo '<br>';
 
+//Example 4----------
 $count = 0;
 do {
 	if ($count == 5) {
--- Loops/for_loop.php
+++ Loops/for_loop.php
@@ -1,9 +1,11 @@
 <?php
+//Example 1
 $Array = array("Moshiur", "Mim", "Imtiaz");
 for ($i = 0; $i < count($Array); $i++) {
     echo $Array[$i] . "<br>";
 }
 echo "foreach Loop" . "<br>";
+//Example 3 with foreach_loop
 foreach ($Array as $Value) {
     echo $Value;
 }
