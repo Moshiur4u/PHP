b5cef89c73e28f80ff0bec5bd07c75a191a858cb
Moshiur Rahman <m.sujon4u@gmail.com>
2026-02-07 22:57:33 +0600

    update SwitchCase

--- Condition/SwithCase.php
+++ Condition/SwithCase.php
@@ -1,2 +1,27 @@
 <?php
 $offDay = 3;
+switch ($offDay) {
+    case 1:
+        echo "Sunday";
+        break;
+    case 2:
+        echo "MonDay";
+        break;
+    case 3:
+        echo "tuesDay";
+        break;
+    case 4:
+        echo "wednesDay";
+        break;
+    case 5:
+        echo "thursDay";
+        break;
+    case 6:
+        echo "FriDay";
+        break;
+    case 7:
+        echo "SaturDay";
+        break;
+    default:
+        echo "Invalid Day";
+}
