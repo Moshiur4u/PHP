f7e2094636408a12b2bf8cd8ab3cbac7144cf7df
Moshiur Rahman <m.sujon4u@gmail.com>
2026-02-17 13:31:21 +0600

    Update <Pre> tag in foreach

--- Loops/Break_Statment/foreach.php
+++ Loops/Break_Statment/foreach.php
@@ -2,12 +2,16 @@
 
 $array = ["Moahiur", "Mim", "Imtiaz"];
 foreach ($array as $print) {
+    echo "<pre>";
     echo $print . "\n";
+    echo "</pre>";
 };
 echo "<br>";
 $array2 = ['Moshiur=32', "Mim=24", "Imtiaz=3"];
 foreach ($array2 as $key => $value) {
     // echo "Age Is : - $value";
     echo "<hr>";
+    echo "<pre>";
     echo "$key . $value";
+    echo "</pre>";
 }
