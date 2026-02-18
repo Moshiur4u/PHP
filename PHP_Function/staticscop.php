65993d5ae895b8b74fd3b1c6f04d58e85700bfa1
Moshiur Rahman <m.sujon4u@gmail.com>
2026-02-18 09:35:49 +0600

    add echo in statstic

--- PHP_Function/staticscop.php
+++ PHP_Function/staticscop.php
@@ -1,12 +1,13 @@
 <?php
 // একটা প্রোগ্রামে ফংশন কত বার কাল করা হইছে তা জানার জন্য 
 $count = 0;
-function countx(){
+function countx()
+{
     global $count;
     $count++;
 }
-countx ();
-countx ();
-countx ();
-countx ();
-countx ();
+echo countx();
+echo countx();
+echo countx();
+echo countx();
+echo countx();
