dee1c63fb0e08b6a1471760a5f859d6f5b4906ec
Moshiur Rahman <m.sujon4u@gmail.com>
2026-03-08 14:34:45 +0600

    add Commnet and work widthwar Opttion

--- Class/class.account.php
+++ Class/class.account.php
@@ -3,19 +3,42 @@ class Account
 {
 
     public $accountNumber;
-    public $blanch;
-
-    function __construct($accountNumber, $blanch)
+    public $balanch;
+    // construct এর মাধ্যমে  একাধিক ভারিয়াব্লে এর কাজ করা হয়।
+    function __construct($accountNumber, $balanch)
     {
         $this->accountNumber = $accountNumber;
-        $this->blanch = $blanch;
+        $this->balanch = $balanch;
     }
-    function getBlanch()
+    function getBalanch()
     {
-        return $this->blanch;
+        return $this->balanch;
     }
 
+    function deposit($amount)
+    {
+        $this->balanch += $amount;
+    }
+
+    // উইথড্রলের জন্য লজিক কাজ করবে কারন  এখানে ব্যলেন্স চেক করতে হবে তার পর উত্তলন দিতে হবে।
+    function withdraw($amount)
+    {
+        if ($amount > $this->balanch) {
+            echo "Insaficiance balanch";
+            return;
+        }
+        $this->balanch -= $amount;
+    }
 }
 $MoshiurAccount = new Account("525020", 20000);
+echo $MoshiurAccount->getBalanch();
+
+$MoshiurAccount->deposit(5000);
+echo PHP_EOL;
+echo $MoshiurAccount->getBalanch();
+
+$MoshiurAccount->withdraw(7000);
+echo PHP_EOL;
+echo $MoshiurAccount->getBalanch();
 
 ?>
