0fe3b254880196631dc047eb1b6acac32714b9fe
Moshiur Rahman <m.sujon4u@gmail.com>
2026-03-13 23:18:53 +0600

    Create a file using RAW PHP

--- file1.php
+++ file1.php
@@ -6,9 +6,13 @@ $books = [
     "Islam",
     "socalsceince",
 ];
-$file = fopen("$books.text","w");
-fwrite($file,$books[0].PHP_EOL);
-fwrite($file,$books[1].PHP_EOL);
-fwrite($file,$books[2].PHP_EOL);
-fwrite($file,$books[3].PHP_EOL);
-fwrite($file,$books[4].PHP_EOL);
+$file = fopen("books.text","w");
+//fwrite($file,$books[0].PHP_EOL);
+//fwrite($file,$books[1].PHP_EOL);
+//fwrite($file,$books[2].PHP_EOL);
+//fwrite($file,$books[3].PHP_EOL);
+//fwrite($file,$books[4].PHP_EOL);
+
+foreach ($books as $book){
+    fwrite($file, $book.PHP_EOL);
+}
